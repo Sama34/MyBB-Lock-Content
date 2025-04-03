38a866a49cc520f495f98c8beea6c14f80c6ff95
Sama34 <Sama34@users.noreply.github.com>
2025-04-03 03:21:04 -0700

    META 1.8.37

--- Upload/inc/plugins/lock.php
+++ Upload/inc/plugins/lock.php
@@ -58,8 +58,8 @@ function lock_info(): array
         'website' => 'https://ougc.network',
         'author' => 'Omar G.',
         'authorsite' => 'https://ougc.network',
-        'version' => '1.8.36',
-        'versioncode' => 1836,
+        'version' => '1.8.37',
+        'versioncode' => 1837,
         'compatibility' => '18*',
         'codename' => 'ougc_lock',
         'pl' => [
